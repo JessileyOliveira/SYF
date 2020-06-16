80d4c1c1bdf887d6bacc7f74da79a12f6e87e871
Jessiley <xjaco222x@hotmail.com>
2020-06-16 10:55:52 -0300

    Token variable

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-const token =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZDYzMDAwMTM3YzllYjYyMWYzYzJlZTNlODE2YmYxMCIsInN1YiI6IjVlZTY1NTNiYWZhMWIwMDAyMmMwMGYwNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PeecqzoN8xtga4AIdwY81zAm4_loQ7bneXz8-fT93O8';
+const token = 'Your token here';
 
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org',
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZDYzMDAwMTM3YzllYjYyMWYzYzJlZTNlODE2YmYxMCIsInN1YiI6IjVlZTY1NTNiYWZhMWIwMDAyMmMwMGYwNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PeecqzoN8xtga4AIdwY81zAm4_loQ7bneXz8-fT93O8`,
+    Authorization: `Bearer ${token}`,
   },
 });
 
